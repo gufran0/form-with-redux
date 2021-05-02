f60c76c9396869671503ee02db1799e4af01ae0c
Md Gufran <68150214+gufran0@users.noreply.github.com>
2021-05-02 20:16:30 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Login from './Components/Login'
+import "./App.css"
+import { useSelector } from 'react-redux'
+import { selectUser } from './features/userSlice'
+import Logout from './Components/Logout'
+
+
+const App = () => {
+    const user = useSelector(selectUser);
+    return (
+        <div>
+            {user ? <Logout></Logout> : <Login></Login>}
+        </div>
+    )
+}
+
+export default App
